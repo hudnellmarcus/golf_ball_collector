241f92c221cbef98b161c1133878d3e12a5a98d1
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2022-07-07 00:11:09 -0700

    completed models

--- golf_ball_app/admin.py
+++ golf_ball_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Ball
+from .models import Attribute
 
 # Register your models here.
+admin.site.register(Ball)
+admin.site.register(Attribute)
--- golf_ball_app/models.py
+++ golf_ball_app/models.py
@@ -1,6 +1,64 @@
 from django.db import models
 
+BRANDS = [
+    ('B', 'Bridgestone'),
+    ('C', 'Callaway'),
+    ('H', 'Honma'),
+    ('K', 'Kirkland'),
+    ('M', 'Mizuno'),
+    ('Sl', 'Slazenger'),
+    ('Sr', 'Srixon'),
+    ('Ta', 'Taylormade'),
+    ('Ti', 'Titleist'),
+    ('To', 'TopFlite'),
+    ('Vi', 'Vice'),
+    ('Vo', 'Volvik'),
+    ('W', 'Wilson'),
+]
+
+SPIN = [
+    ('L', 'Low'),
+    ('M', 'Medium'),
+    ('H', 'High')
+]
+
+
+FEEL = [
+    ('S', 'Soft'),
+    ('F', 'Firm')
+]
+
+
 # Create your models here.
-class Golf_Ball(models.Model):
-    brand = models.CharField(max_length=100)
-  
+class Ball(models.Model):
+
+    brand = models.CharField(
+        max_length=2,
+        choices=BRANDS
+    )
+    name = models.CharField(max_length=100, default="")
+    description = models.TextField(max_length=250)
+    
+    def __str__(self):
+        return f"{self.brand}:{self.name}"
+    
+class Attribute(models.Model):
+    
+    spin = models.CharField(
+        max_length=1,
+        choices=SPIN
+    )
+    
+    feel = models.CharField(
+        max_length=1,
+        choices=FEEL
+    )
+    
+    ball = models.ForeignKey(Ball, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.ball} spin:{self.spin} feel:{self.feel}"
+    
+    
+    
+    
